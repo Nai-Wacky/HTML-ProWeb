2ef7ad174b0ebead141a39a90d767bac8390dbf4
Nai-Wacky <contaricardohernandez@gmail.com>
2025-04-21 13:58:41 -0600

    acomodo

--- Proyecto web/phps/endPointLogin.php
+++ Proyecto web/phps/endPointLogin.php
@@ -4,7 +4,9 @@ require "DBManager.php";
 
 if (isset($_POST['corr']) && isset($_POST['pass'])) {
     $db = new DBManager();
+
     $resultado = $db->login($_POST['corr'], $_POST['pass']);
+
     echo json_encode($resultado);
 } else {
     die('Error, se requiere el correo y pass');
