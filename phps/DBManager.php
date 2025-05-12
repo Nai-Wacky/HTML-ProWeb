d5868bb3ac4cde8b0538c20a557948a008754f9a
Nai-Wacky <contaricardohernandez@gmail.com>
2025-05-12 13:40:27 -0600

    cambios dbmanager

--- phps/DBManager.php
+++ phps/DBManager.php
@@ -4,6 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require "Usuarios.php";
+require "Direccion.php";
 
 class DBManager
 {
@@ -106,34 +107,182 @@ class DBManager
         return $resultado;
     }
 
+    public function editNombre($id_usuario, $nombre) //FUNCIONA
+    {
+        $link = $this->open();
+
+        $sql = "UPDATE usuarios SET nombre = ? WHERE id_usuario = ?";
+
+        $query = mysqli_prepare($link, $sql);
+
+        mysqli_stmt_bind_param(
+            $query,
+            "si",
+            $nombre,
+            $id_usuario
+        );
+
+        $resultado = mysqli_stmt_execute($query) or die('Error update');
+
+        $this->close($link);
+
+        return $resultado;
+    }
+
+    public function editPassword($id_usuario, $password) //FUNCIONA
+    {
+        $link = $this->open();
+
+        $sql = "UPDATE usuarios SET password = ? WHERE id_usuario = ?";
+
+        $query = mysqli_prepare($link, $sql);
+
+        mysqli_stmt_bind_param(
+            $query,
+            "si",
+            $password,
+            $id_usuario
+        );
+
+        $resultado = mysqli_stmt_execute($query) or die('Error update');
+
+        $this->close($link);
+
+        return $resultado;
+    }
+
+
+
     //-------------------------------------------------------------------direcciones-------------------------------------------------------------------
 
+    public function getDirecciones()
+    {
+        $link = $this->open();
+
+        $sql = "SELECT * FROM direcciones"; // Ajusta el nombre de la tabla si es diferente
+        $result = mysqli_query($link, $sql);
+
+        $productos = [];
+
+        if ($result) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $productos[] = $row;
+            }
+        } else {
+            return ["error" => "Error al obtener los productos"];
+        }
+
+        $this->close($link);
+
+        return $productos;
+    }
 
 
+    public function addAddress(Direccion $direccion) //ADAPTADO PARA EL PROYECTO
+    {
+        $link = $this->open();
+
+        $sql = "INSERT INTO direcciones VALUES(NULL, ?, ?, ?, ?, ?, ?, ?)";
+
+        // Prepara la consulta
+        $query = mysqli_prepare($link, $sql);
+
+        // Enlaza los parametros (reemplaza comodines)
+        // Tipos: i para enteros, s para string, d para double y b para blob
+
+        //print $usuario->nombre;
+
+        mysqli_stmt_bind_param(
+            $query,
+            "issssss",
+            $direccion->id_usuario,
+            $direccion->estado,
+            $direccion->municipio,
+            $direccion->cod_postal,
+            $direccion->calle,
+            $direccion->colonia,
+            $direccion->numero
+        );
+
+        // Ejecuta la query
+        $resultado = mysqli_stmt_execute($query) or die('Error insert');
+
+        $this->close($link);
+
+        return $resultado;
+    }
+
+    public function deleteDireccion($id_direccion)
+    {
+        $link = $this->open();
+
+        $sql = "DELETE FROM direcciones WHERE id_direccion = ?";
+
+        $query = mysqli_prepare($link, $sql);
+
+        mysqli_stmt_bind_param(
+            $query,
+            "i",
+            $id_direccion
+        );
+
+        $resultado = mysqli_stmt_execute($query) or die('Error delete');
+
+        $this->close($link);
+
+        return $resultado;
+    }
+
+    public function editDireccion(Direccion $direccion) //FUNCIONA
+    {
+        $link = $this->open();
+
+        $sql = "UPDATE direcciones SET id_usuario= ? ,estado= ? ,municipio= ? ,cod_postal= ? ,calle= ? ,colonia= ? ,numero= ? WHERE id_direccion = ?";
+
+        $query = mysqli_prepare($link, $sql);
+
+        mysqli_stmt_bind_param(
+            $query,
+            "issssssi",
+            $direccion->id_usuario,
+            $direccion->estado,
+            $direccion->municipio,
+            $direccion->cod_postal,
+            $direccion->calle,
+            $direccion->colonia,
+            $direccion->numero,
+            $direccion->id_direccion
+        );
+
+        $resultado = mysqli_stmt_execute($query) or die('Error update');
+
+        $this->close($link);
+
+        return $resultado;
+    }
 
 
     //-------------------------------------------------------------------productos-------------------------------------------------------------------
 
     public function getProductos()
-{
-    $link = $this->open();
+    {
+        $link = $this->open();
 
-    $sql = "SELECT * FROM productos"; // Ajusta el nombre de la tabla si es diferente
-    $result = mysqli_query($link, $sql);
+        $sql = "SELECT * FROM productos"; // Ajusta el nombre de la tabla si es diferente
+        $result = mysqli_query($link, $sql);
 
-    $productos = [];
+        $productos = [];
 
-    if ($result) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            $productos[] = $row;
+        if ($result) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                $productos[] = $row;
+            }
+        } else {
+            return ["error" => "Error al obtener los productos"];
         }
-    } else {
-        return ["error" => "Error al obtener los productos"];
+
+        $this->close($link);
+
+        return $productos;
     }
-
-    $this->close($link);
-
-    return $productos;
-}
-
 }
---
+++ phps/Direccion.php
@@ -0,0 +1,15 @@
+<?php
+
+class Direccion
+{
+    public $id_direccion;
+    public $id_usuario;
+    public $estado;
+    public $municipio;
+    public $cod_postal;
+    public $calle;
+    public $colonia;
+    public $numero;
+
+    public function __construct() {}
+}
