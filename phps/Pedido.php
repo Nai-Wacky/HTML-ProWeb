c9ba727173735139e80567c6e44d25839624b1b7
Nai-Wacky <contaricardohernandez@gmail.com>
2025-05-22 06:09:33 -0600

    nada

---
+++ phps/Pedido.php
@@ -0,0 +1,14 @@
+<?php
+
+class Pedido
+{
+    public $id_pedido;
+    public $id_usuario;
+    public $id_direccion;
+    public $id_producto;
+    public $cantidad;
+    public $fecha;
+    public $status;
+
+    public function __construct() {}
+}
